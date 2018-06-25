ae3ab9d752617c2028e4cf2cec9fa92e84c41679
bobSpacewalk <32118516+bobSpacewalk@users.noreply.github.com>
2018-06-25 13:41:58 +0900

    Add files via upload

--- 180323_fucntions for postgresql_euc-kr.r
+++ 180323_fucntions for postgresql_euc-kr.r
@@ -24,7 +24,7 @@ dbGetTable = function(text, data.table=FALSE) {
 }
 
 dbSetTable = function(database=database_name, table=table_name, data=data, overwrite=FALSE, append=FALSE) {
-  dbCon = dbChange(database)
+  dbCon <<- dbChange(database)
   return(dbWriteTable(dbCon, table, data, row.names=FALSE, overwrite=overwrite, append=append))
 }
 
@@ -32,12 +32,12 @@ dbChange = function(text) {
   dbDisconnect(dbCon)
   for(i in 1:10) {
     tryCatch({
-      dbCon = dbConnect(dbDriver("PostgreSQL"), dbname = text, host = "spwk-dw.cicvuwhjlhxo.ap-northeast-2.rds.amazonaws.com", port = 5432, user = "root", password = Sys.getenv('AWS_PGS_PW'))
+      dbCon <<- dbConnect(dbDriver("PostgreSQL"), dbname = text, host = "spwk-dw.cicvuwhjlhxo.ap-northeast-2.rds.amazonaws.com", port = 5432, user = "root", password = Sys.getenv('AWS_PGS_PW'))
       cat(text, '연결\n')
       return(dbCon)
       },
-      error = function(e) cat('error(', i, ')'),
-      warning = function(w) cat('warning(', i, ')')
+      error = function(e) cat('error(', i, ') '),
+      warning = function(w) cat('warning(', i, ') ')
     )
   }
   cat('\n연결실패\n')
@@ -59,6 +59,36 @@ dbTableList = function() {
 
 dbDescription = function() {
   browseURL('https://docs.google.com/spreadsheets/d/1uztusrcpc8_5nf6icbvERVhkkZNjEkk-LXV49vM3eto/edit#gid=0', browser = getOption('browser'))
+}
+
+dbAppendGoogleSheetList = function() {
+  return()
+}
+
+dbUpdateGoogleSheet = function() {
+  # dbChange('workspace')
+  # sheet_url = dbGetTable('select * from googlesheet_url')
+  sheet_url = data.frame(
+    database = 'workspace',
+    table = c('category_own', 'category_landuse_zone', 'category_bld_use', 'category_strc', 'category_jimok', 'data_rule'),
+    url = c(
+       'https://docs.google.com/spreadsheets/d/e/2PACX-1vReYPG62NNOt1O58SxycguyqzoaQf_irgbh6hkfgMphixRzpJJj2COPfKg1jR6UputSUiCstXoZ4f42/pub?gid=0&single=true&output=csv',
+       'https://docs.google.com/spreadsheets/d/e/2PACX-1vSG92AzpS9yQNK45mRarjtTl8sSS6537y4MKgwlDu3it6VLw5ZYBhI_8C_p5hIBphveFK8gGVPvnT7d/pub?gid=0&single=true&output=csv',
+       'https://docs.google.com/spreadsheets/d/e/2PACX-1vS8i0ebCOtCKa1WjqIllu6quzUfqHaiwbwkfUl5PMJPSjatpRC5TEWWwPQXxTdYOqBz-dPLd3PwFG6B/pub?gid=2012344268&single=true&output=csv',
+       "https://docs.google.com/spreadsheets/d/e/2PACX-1vTmiRDkR-Me8d6exSjlgIXdVJ1ZvLBZ57ywuBuQTBen5tn1njB5aeuqeQClw7rrGMXD44SAFpcCiimo/pub?gid=0&single=true&output=csv",
+       "https://docs.google.com/spreadsheets/d/e/2PACX-1vTvNbHtfbTm7-jmTFFNtGL1-0QSxFCir23TtyoxW75z1tqyBu9yrNHhljcZLWj4uzYXAVawU_oeVEYT/pub?gid=1693110064&single=true&output=csv",
+       "https://docs.google.com/spreadsheets/d/e/2PACX-1vSOAqZJ-7MN1Q2JYI79gb-vribBwoCDjRIn24RtN7_tRgMcTk9Sw8Kq4EJ-syNlVaAl3JANdgXuySJE/pub?gid=0&single=true&output=csv"
+    ),
+    stringsAsFactors=FALSE
+  )
+  for(i in 1:nrow(sheet_url)) {
+    sheet_data = read.csv(sheet_url$url[i], fileEncoding='UTF-8', colClasses='character')
+    if(dbSetTable(sheet_url$database[i], sheet_url$table[i], sheet_data, overwrite=TRUE)) { # update함수이므로 항상 overwrite
+      cat(paste0(sheet_url$table[i], ' is updated\n'))
+    } else {
+      cat(paste0(sheet_url$table[i], ' fails update\n'))
+    }
+  }
 }
 
 db_help = function() {

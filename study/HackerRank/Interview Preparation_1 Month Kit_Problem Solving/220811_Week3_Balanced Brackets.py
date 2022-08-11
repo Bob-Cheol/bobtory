f6c83ba80cd47a8beed67062e16896e63c4091b9
BobPark <bobpark@spacewalk.tech>
2022-08-11 21:46:55 +0900

    #1 done Balanced Brackets

--- study/HackerRank/Interview Preparation_1 Month Kit_Problem Solving/220811_Week3_Balanced Brackets.py
+++ study/HackerRank/Interview Preparation_1 Month Kit_Problem Solving/220811_Week3_Balanced Brackets.py
@@ -15,17 +15,44 @@ import sys
 
 def isBalanced(s):
     # Write your code here
+    def get_balanced_bracket(left_bracket):
+        if left_bracket == '(':
+            return ')'
+        elif left_bracket == '{':
+            return '}'
+        elif left_bracket == '[':
+            return ']'
+        else:
+            return None
+
+    s = list(s)
+    pnt = 0
+    while len(s):
+        if s[pnt] in ['(','{','[']:
+            pnt += 1
+            if len(s) == pnt:
+                return 'NO'
+        else:
+            if get_balanced_bracket(s[pnt-1]) != s[pnt]:
+                return 'NO'
+            else:
+                s.pop(pnt-1)
+                s.pop(pnt-1)
+                pnt -= 1
+    return 'YES'
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    input_txt = open('test_case/Balanced Brackets-9.txt', 'r')
 
-    t = int(input().strip())
+    t = int(input_txt.readline().strip())
 
     for t_itr in range(t):
-        s = input()
+        s = input_txt.readline().strip()
 
         result = isBalanced(s)
 
-        fptr.write(result + '\n')
+        # fptr.write(result + '\n')
+        print(result)
 
-    fptr.close()
+    # fptr.close()

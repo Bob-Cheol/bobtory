a71618e89548bb9e81aeec6bd11c10c896192044
unknown <bobpark@spacewalk.tech>
2022-08-11 12:15:53 +0900

    #1 Done Reverse a linked list

--- study/HackerRank/Interview Preparation_1 Month Kit_Problem Solving/220810_Week3_Reverse a linked list.py
+++ study/HackerRank/Interview Preparation_1 Month Kit_Problem Solving/220810_Week3_Reverse a linked list.py
@@ -27,14 +27,16 @@ class SinglyLinkedList:
 
         self.tail = node
 
-def print_singly_linked_list(node, sep, fptr):
+def print_singly_linked_list(node, sep):
     while node:
-        fptr.write(str(node.data))
+        # fptr.write(str(node.data))
+        print(str(node.data), end = '')
 
         node = node.next
 
         if node:
-            fptr.write(sep)
+            # fptr.write(sep)
+            print(sep, end='')
 
 #
 # Complete the 'reverse' function below.
@@ -54,24 +56,40 @@ def print_singly_linked_list(node, sep, fptr):
 
 def reverse(llist):
     # Write your code here
+    reverse_llist = SinglyLinkedList()
+
+    while llist:
+        reverse_node = SinglyLinkedListNode(llist.data)
+        if not reverse_llist.tail:
+            reverse_llist.tail = reverse_node
+        else:
+            reverse_node.next = reverse_llist.head
+
+        reverse_llist.head = reverse_node
+        llist = llist.next
+
+    return reverse_llist.head
+
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    input_txt = open('test_case/Reverse a linked list-0.txt','r')
 
-    tests = int(input())
+    tests = int(input_txt.readline())
 
     for tests_itr in range(tests):
-        llist_count = int(input())
+        llist_count = int(input_txt.readline())
 
         llist = SinglyLinkedList()
 
         for _ in range(llist_count):
-            llist_item = int(input())
+            llist_item = int(input_txt.readline())
             llist.insert_node(llist_item)
 
         llist1 = reverse(llist.head)
 
-        print_singly_linked_list(llist1, ' ', fptr)
-        fptr.write('\n')
+        print_singly_linked_list(llist1, ' ')
+        # fptr.write('\n')
 
-    fptr.close()
+    # fptr.close()
+    input_txt.close()

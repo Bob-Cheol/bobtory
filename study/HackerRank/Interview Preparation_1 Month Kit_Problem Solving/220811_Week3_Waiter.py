aec4170e5869eda1fccb22901c3b4efffe9d5511
unknown <bobpark@spacewalk.tech>
2022-08-12 16:18:55 +0900

    #1 done Waiter

--- study/HackerRank/Interview Preparation_1 Month Kit_Problem Solving/220811_Week3_Waiter.py
+++ study/HackerRank/Interview Preparation_1 Month Kit_Problem Solving/220811_Week3_Waiter.py
@@ -17,11 +17,39 @@ import sys
 
 def waiter(number, q):
     # Write your code here
-    return
+    def get_prime_number():
+        prime_number_list = [2]
+        num = 2
+
+        while len(prime_number_list) <= 1200:
+            num += 1
+            while True:
+                for i in range(2,num):
+                    if num % i == 0:
+                        break
+                    elif i == num - 1:
+                        prime_number_list.append(num)
+                break
+
+        return prime_number_list
+    prime_number_list = get_prime_number()
+    answer = list()
+    for i in range(q):
+        delete_num = list()
+        for n in range(len(number)):
+            if number[n] % prime_number_list[i] == 0:
+                answer.append(number[n])
+                delete_num.append(n)
+        for d in delete_num[::-1]:
+            number.pop(d)
+        if i != q - 1:
+            number = number[::-1]
+    answer = answer + number
+    return answer
 
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    input_txt = open('test_case/Waiter-0.txt', 'r')
+    input_txt = open('test_case/Waiter-1.txt', 'r')
 
     first_multiple_input = input_txt.readline().rstrip().split()
 

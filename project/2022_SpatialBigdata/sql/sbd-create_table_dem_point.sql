adbbd5cf0817b28bbbe3073f87fd00641a276d68
unknown <bobpark@spacewalk.tech>
2022-09-28 00:33:55 +0900

    #17 done uploading lot_polygon

--- project/2022_SpatialBigdata/sql/sbd-create_table_dem_point.sql
+++ project/2022_SpatialBigdata/sql/sbd-create_table_dem_point.sql
@@ -1,7 +1,7 @@
 create table dem_point (
   id int,
   value numeric,
-  geometry public.geometry(geometry,5174),
+  geometry public.geometry(geometry, 5179),
   uploaded_at timestamp default current_timestamp
 );
 create index index_dem_point_on_geometry on dem_point (geometry);
